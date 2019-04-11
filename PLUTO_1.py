e3e33492bb289dfaf9a17aac57e2994e7ae68725
vidit_jain <viditjain5598@gmail.com>
2019-04-11 11:33:25 +0530

    improved parser dictionary

--- PLUTO_1.py
+++ PLUTO_1.py
@@ -12,6 +12,13 @@ PLUTO_code = """
                         variable CMD_TM_LINK_VALUE of type string
                         variable TRSP2_RECEIVER_STATUS of type string
                     end declare
+        
+                    CMD_TM_LINK_VALUE := "TM FLOW";
+                    TRSP2_RECEIVER_STATUS := "TC tracking";
+
+                    if CMD_TM_LINK != CMD_TM_LINK_VALUE then
+                        log "There is no TM FLOw.";
+                    end if;
                 end step;
             end procedure
         """
@@ -45,9 +52,15 @@ end procedure
 PLUTO_grammar = """
     start : procedure
     procedure: "procedure" procedure_body "end procedure"  -> procedure_body
-    procedure_body: "initiate and confirm step" STRING [declare_body] "end step;" ->step_name
+    procedure_body: "initiate and confirm step" STRING [declare_body] (assign_command | if_cond)+ "end step;" ->step_name
     declare_body: "declare" var_declaration+ "end declare"
-    var_declaration: "variable" STRING "of type" STRING 
+    var_declaration: "variable" STRING "of type" STRING
+    assign_command: STRING ":=" ESCAPED_STRING ";"
+    if_cond: "if" condition "then" exec_comm "end if;"
+    condition: STRING ("!=" | "=") STRING
+    exec_comm: "log" ESCAPED_STRING ";"
+
+    %import common.ESCAPED_STRING  
     %import common.CNAME -> STRING
     %import common.NUMBER -> NUMBER
     %import common.WS
